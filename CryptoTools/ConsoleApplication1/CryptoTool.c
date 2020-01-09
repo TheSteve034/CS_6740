1afb332d75aff19de8df552c58fc14d331fbf3ce
Samuel Reeder <swreeder88@gmail.com>
2020-01-09 15:51:49 -0700

    set up secure method for accepting command line input

--- CryptoTools/ConsoleApplication1/CryptoTool.c
+++ CryptoTools/ConsoleApplication1/CryptoTool.c
@@ -1,18 +1,38 @@
 #include<stdio.h>
-
-int main() {
-	char buffer[50] = { 0 };
-	int a = 10, b = 20, c = 0;
-	c = a + b;
-	snprintf(buffer, 50, "%d", c);
-	printf("Here is some stuff in the buffer:\n");
-	for (int i = 0; i < 50; i++) {
-		if (buffer[i] != '\0') {
-			printf("%c", buffer[i]);
+#include <stdbool.h>
+void clearStdIn() {
+	int x = 0;
+	while ((x = getchar()) != '\n' && x != EOF) {}
+}
+//this processes the useres input
+void mainMenuSelection() {
+	printf("Press 1 for the simple crypto tool.\n");
+	//create a char array and set it to NULL
+	char buffer[2] = { 0 };
+	//only allow 1 char from the user.
+	fgets(buffer, 2, stdin);
+	//validate the users 
+	bool isValid = false;
+	while (isValid == false) {
+		if (buffer[0] != '1') {
+			printf("Invalid selection. Press 1 for the simple crypto tool\n");
+			clearStdIn();
+			fgets(buffer, 2, stdin);
 		}
-		else {
-			break;
+		else if (buffer[0] == '1') {
+			isValid = true;
 		}
 	}
+}
+//This routine sets up the main main menu
+void setupMainMenu() {
+	printf("------------------------------------\n");
+	printf("Welcome to the Crypto Tools Suite\n");
+	mainMenuSelection();
+	printf("------------------------------------\n");
+}
+
+int main() {
+	setupMainMenu();
 	return 0;
 }
